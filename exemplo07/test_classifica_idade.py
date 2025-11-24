72f19fa50c7c1ee341ab81351b08b3beec87901b
gutinferreira <gutinferreira97@gmail.com>
2025-11-24 12:10:15 +0000

    exemplos parametrize

---
+++ exemplo07/test_classifica_idade.py
@@ -0,0 +1,13 @@
+import pytest
+from classifica_idade import classifica_idade
+
+@pytest.mark.parametrize("idade,categoria_esperada",[
+    (5,"Criança"),
+    (15,"Adolescente"),
+    (30,"Adulto"),
+    (60,"Idoso")
+
+])
+
+def test_classifica_idade(idade,categoria_esperada):
+    assert classifica_idade(idade) == categoria_esperada
